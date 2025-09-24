b98b25b032198d7665fe1c0acc55e5deb4dad20c
DernJoodRandomMetronome <devnull@example.com>
2025-09-24 22:47:19 +0700

    v0.1

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,8 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
 	plugins: [react()],
+	// Set base for GitHub Pages deployment under repo path
+	base: '/DernJoodRandomMetronome/',
 });
 
 
